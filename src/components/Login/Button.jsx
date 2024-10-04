0d64cfba885d0fbc718f1a967723d67bfb4cc3f4
Louise Eriksson <louiseriksson.92@gmail.com>
2024-10-04 12:18:38 +0200

    ekdm

--- src/components/Login/Button.jsx
+++ src/components/Login/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./Login.module.css";
 
-const Button = ({ btnTitle, style, className }) => {
+const Button = ({ btnTitle, style, btnClassName }) => {
   return (
     <div className={styles.buttonContainer}>
-      <button style={style} className={`${styles.btn} ${className}`}>
+      <button style={style} className={`${styles.btn} ${btnClassName}`}>
         {btnTitle}
       </button>
     </div>
